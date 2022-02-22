5925daafd0429d8f8f07a2fa79def7ea97904927
Luis Sanches <luissanches@gmail.com>
2022-02-22 17:28:29 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.sass'
+
+const App = () => {
+	return (
+		<div className="app-main-container">
+			aqui vc vai criar seu app
+		</div>
+	)
+}
+
+export default App
